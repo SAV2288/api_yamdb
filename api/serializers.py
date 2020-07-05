f1690cf4eb76021708373c449f287466814d6a51
Смирнов Алексей <g.o.borneo@yandex.ru>
2020-07-05 20:53:05 +0300

    Improved titles instance creation

--- api/serializers.py
+++ api/serializers.py
@@ -6,20 +6,22 @@ from api.models import Categories
 
 
 class GenreSerialiser(serializers.ModelSerializer):
+    slug = serializers.SlugField(required=False)
     class Meta:
         fields = ('name', 'slug')
         model = Genres
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(required=False)
     class Meta:
         fields = ('name', 'slug')
         model = Categories
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    genre = GenreSerialiser(many=True, required=False)
-    category = CategorySerializer(required=False)
+    genre = GenreSerialiser(many=True, required=False, read_only=True)
+    category = CategorySerializer(required=False, read_only=True)
 
     class Meta:
         fields = ('id', 'name', 'year', 'description', 'genre', 'category')
